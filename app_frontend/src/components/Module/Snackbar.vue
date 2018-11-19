7d3ba4fa3ab3ceda779ff97db07293269e4ebaa1
yuji0320 <yuji0320@users.noreply.github.com>
2018-11-19 18:34:37 +0900

    フロントエンド会社マスタ編集画面開発完了

---
+++ app_frontend/src/components/Module/Snackbar.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <!-- <v-card>
+      <v-card-text>
+        <v-btn
+          block
+          color="primary"
+          dark
+          @click="showSnackbar('test')"
+        >
+          Show Snackbar
+        </v-btn>
+      </v-card-text>
+    </v-card> -->
+    <v-snackbar
+      :top=true
+      :value="snackbar.snackbarStatus"
+      :color="snackbar.color"
+      :timeout="snackbar.timeout"
+    >
+      {{ snackbar.snack }}
+      <v-btn
+        dark
+        flat
+        @click="snackbar.snackbarStatus = false"
+      >
+        Close
+      </v-btn>
+    </v-snackbar>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  name: "snackbar",
+  data() {
+    return {
+      color: "success"
+    };
+  },
+  computed: {
+    ...mapState("systemConfig", ["snackbar"])
+  }
+};
+</script>
---
+++ app_frontend/src/components/Pages/Master/Staff.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-container 
+      fluid
+      grid-list-lg
+    >
+    <v-card>
+      <v-toolbar card>
+        <v-icon>people</v-icon>
+        <v-toolbar-title class="font-weight-light">Staff Master</v-toolbar-title>
+      </v-toolbar>
+
+      {{ userStaffs }}
+
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+import { mapState, mapActions } from "vuex";
+
+export default {
+  title: "Staff master",
+  name: "Staff",
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapState("auth", ["loginUserData"]),
+    ...mapState("systemUserApi", ["userStaffs"])
+  },
+  methods: {
+    ...mapActions("systemUserApi", ["getStaffs"])
+  },
+  mounted() {
+    this.getStaffs({ params: {company : this.loginUserData.companyId} });
+  }
+};
+</script>
