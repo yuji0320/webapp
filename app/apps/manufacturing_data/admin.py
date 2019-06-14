835b24891ad8f099af713cb2271fbc958cf00076
yuji0320 <yuji0320@users.noreply.github.com>
2019-06-14 17:07:08 -0400

    バックエンド、django admin修正

--- app/apps/manufacturing_data/admin.py
+++ app/apps/manufacturing_data/admin.py
@@ -10,7 +10,15 @@ class AdminBillOfMaterial(admin.ModelAdmin):
     list_display = ('name', 'created_at', 'modified_at')
 
 
+class AdminMakingOrder(admin.ModelAdmin):
+    list_display = ('number', 'name', 'created_at', 'modified_at')
+
+
+class AdminReceivingProcess(admin.ModelAdmin):
+    list_display = ('order', 'created_at', 'modified_at')
+
+
 admin.site.register(JobOrder, AdminJobOrder)
 admin.site.register(BillOfMaterial, AdminBillOfMaterial)
-admin.site.register(MakingOrder)
-admin.site.register(ReceivingProcess)
+admin.site.register(MakingOrder, AdminMakingOrder)
+admin.site.register(ReceivingProcess, AdminReceivingProcess)
